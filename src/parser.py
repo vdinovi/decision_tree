75c7c68dce866eb8dd07a6f365810a73ed64168c
vdinovi <vito.dinovi@gmail.com>
2018-04-19 22:19:01 -0700

    added comments

--- src/parser.py
+++ src/parser.py
@@ -5,6 +5,7 @@ import csv
 import random
 from pprint import pprint
 
+# Parse the schema XML file -> dictionary
 def parse_schema(filename):
     xml = ET.parse(args.schema_file)
     root = xml.getroot()
@@ -13,6 +14,7 @@ def parse_schema(filename):
         schema[var.attrib['name']] = [ v.attrib for v in var ]
     return schema
 
+# Parse the data file into appropriate format
 def parse_data(filename):
     data = []
     attributes = None
@@ -28,19 +30,22 @@ def parse_data(filename):
         for row in reader:
             row = [int(n) for n in row]
             k = row[class_col]
+            # strip out non-attribute columns and the class column from the data
             for i in range(0, len(row)):
                 if attributes[i][1] < 0 or i == class_col:
                     row[i] = None
             row = [n for n in row if n != None]
+            # store data elements in the form: (data_vector, class)
             data.append((tuple(row), k))
+        # strip out non-attribute columns and the class column from the attributes
         attributes = [a for a in attributes if a[1] >= 0 and a[0] != klass]
     return data, attributes, klass
 
 class Node:
-    NODE_TYPE = ['attribute', 'class']
+    NODE_TYPES = ['attribute', 'class']
 
     def __init__(self, node_type, label=None):
-        if node_type not in self.NODE_TYPE:
+        if node_type not in self.NODE_TYPES:
             raise "'{}' is not a valid node type".format(node_type)
         self.node_type = node_type
         self.label = label
@@ -60,14 +65,16 @@ def generate(D, A, threshold):
     if all(d[1] == klass for d in D):
         return Node("class", klass)
     elif all(a is None for a in A):
-        # TODO picks first klass in D, should find most frequent
+        # TODO picks first klass in D, should find plurality instead
         return Node("class", klass)
     else:
-        # TODO selects random attribute, should find correct one
+        # TODO selects random splitting attribute, should use correct algorithm instead
         selected_idx = random.randint(0, len(A) - 1)
         while not A[selected_idx]:
             selected_idx = random.randint(0, len(A) - 1)
 
+        # The selected attribute is set to None in the attribute list
+        # this preserves the capability to parallel index into data vector
         selected = A[selected_idx]
         A[selected_idx] = None
         node = Node("attribute", selected[0])
