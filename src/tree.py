30383da7f9c72643e45d7f1aa95f33b2496a9231
Billy Gottenstrater <celestite4@yahoo.com>
2018-05-03 17:21:19 -0700

    a

--- src/tree.py
+++ src/tree.py
@@ -47,7 +47,35 @@ def dominant_class(D):
             classes[d[1]] = 1
     return max(classes)
 
-def generate(D, A, threshold, gain_ratio):
+def generateForIris(D, threshold, gain_ratio):
+    A = [('SepalLength', None), ('Sepal Width', None), ('Petal Length', None), ('Petal Width', None)]
+    klass = D[0][1]
+    if all(d[1] == klass for d in D):
+        return Node("class", klass)
+    elif all(a is None for a in A):
+        klass = dominant_class(D)
+        return Node("class", klass)
+    else:
+        selected_attr, selected_idx = selectSplittingAttribute(A,D,threshold, gain_ratio,)
+        if not selected_idx:
+            klass = dominant_class(D)
+            return Node("class", klass)
+        #selected_idx = random.randint(0, len(A) - 1)
+        #while not A[selected_idx]:
+        #    selected_idx = random.randint(0, len(A) - 1)
+
+        # The selected attribute is set to None in the attribute list
+        # this preserves the capability to parallel index into data vector
+        selected = A[selected_idx]
+        A[selected_idx] = None
+        node = Node("attribute", selected[0])
+        for e in range(1, selected[1] + 1):
+            new_data = [d for d in D if d[0][selected_idx] == e]
+            if new_data:
+                node.children[e] = generate(new_data, list(A), threshold, gain_ratio)
+        return node
+
+def generate(D, A, threshold, gain_ratio,numeric=False):
     klass = D[0][1]
     if all(d[1] == klass for d in D):
         return Node("class", klass)
